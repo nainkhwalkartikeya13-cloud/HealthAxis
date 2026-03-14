98398eb801e426e3b97d5995dfa329f547fffa2a
nainkhwalkartikeya13-cloud <nainkhwalkartikeya13@gmail.com>
2026-03-15 03:55:36 +0530

    Update doctorController.js

--- backend/controllers/doctorController.js
+++ backend/controllers/doctorController.js
@@ -13,8 +13,8 @@ const razorpayInstance = new razorpay({
 
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
-  port: process.env.SMTP_PORT,
-  secure: false, // true for 465, false for other ports
+  port: Number(process.env.SMTP_PORT),
+  secure: Number(process.env.SMTP_PORT) === 465, // true for 465, false for other ports
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
